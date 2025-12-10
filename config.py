2979530d0d93977cec5177f7d3056d9847ee99ab
Anastasiia <starschinowa.ana@gmail.com>
2025-12-10 16:46:19 +0100

    edits

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_PATH = f"/{BOT_TOKEN}"
+TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_PATH = f"/{TOKEN}"
 WEBHOOK_URL = os.getenv("WEBHOOK_URL") + WEBHOOK_PATH
+
+# WEBHOOK_PATH = f"/{TOKEN}"
+# WEBHOOK_URL = f"https://familybudgetbot-production.up.railway.app{WEBHOOK_PATH}"
