7b5bd001a056773726f4879a4f9ac2f8485270da
Anastasiia <starschinowa.ana@gmail.com>
2025-12-13 15:05:29 +0100

    edit config.py

--- config.py
+++ config.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
+
 WEBHOOK_PATH = f"/{TOKEN}"
-WEBHOOK_URL = os.getenv("WEBHOOK_URL") + WEBHOOK_PATH
+WEBHOOK_URL = f"https://analyticsbot-production.up.railway.app{WEBHOOK_PATH}"
+
+
 DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
